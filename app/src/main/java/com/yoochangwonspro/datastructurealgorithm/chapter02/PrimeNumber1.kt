c84e58f1ad3aac3672410392c2bbb667e1cb45ae
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-03-12 20:28:27 +0900

    Feat : 소수 판별 알고리즘 연습

--- app/src/main/java/com/yoochangwonspro/datastructurealgorithm/chapter02/PrimeNumber1.kt
+++ app/src/main/java/com/yoochangwonspro/datastructurealgorithm/chapter02/PrimeNumber1.kt
@@ -1,17 +1,40 @@
 package com.yoochangwonspro.datastructurealgorithm.chapter02
 
 fun main(args: Array<String>) {
+//    var count = 0
+//    for (n in 2..50) {
+//        var i = 2
+//        while (i < n) {
+//            count++
+//            if (n % i == 0) break
+//            i++
+//        }
+//        if (n == i) {
+//            println(n)
+//        }
+//    }
+//    println("나숫셈을 수행한 횟수 : $count")
+
+    primeNumberPractice()
+}
+
+fun primeNumberPractice() {
     var count = 0
-    for (n in 2..50) {
-        var i = 2
-        while (i < n) {
+    var primeCnt = 0
+    val prime = Array<Int>(50) { 0 }
+
+    prime[primeCnt++] = 2
+
+    for (n in 3..30 step(2)) {
+        var i = 1
+        while (i < primeCnt) {
             count++
-            if (n % i == 0) break
+            if (n % prime[i] == 0) break
             i++
         }
-        if (n == i) {
-            println(n)
-        }
+        if (i == primeCnt) prime[primeCnt++] = n
     }
-    println("나숫셈을 수행한 횟수 : $count")
+
+    for (i in prime.indices) println(prime[i])
+    println("나눈 수 : $count")
 }
