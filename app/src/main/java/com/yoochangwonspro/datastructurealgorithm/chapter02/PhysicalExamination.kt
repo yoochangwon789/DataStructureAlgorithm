42a6ee8b6e2f6093428c2f8629f81bfe4a131769
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-03-14 04:33:14 +0900

    Feat : 클래스 시력 분포도 구현 알고리즘

--- app/src/main/java/com/yoochangwonspro/datastructurealgorithm/chapter02/PhysicalExamination.kt
+++ app/src/main/java/com/yoochangwonspro/datastructurealgorithm/chapter02/PhysicalExamination.kt
@@ -1,5 +1,7 @@
 package com.yoochangwonspro.datastructurealgorithm.chapter02
 
+import java.util.*
+
 class PhysicalExamination {
 
     data class PhysicalData(val name: String, val height:Int, val vision: Double)
@@ -8,19 +10,39 @@ class PhysicalExamination {
         const val VMAX = 21
 
         // 키으 평균 값을 구하는 함수
-        fun aveHeight(dat: ArrayList<PhysicalData>): Double {
+        fun aveHeight(dat: Array<PhysicalData>): Double {
             var sum: Double = 0.0
-            dat.forEach { physicalData -> sum += physicalData.height }
+            dat.forEach { physicalData -> sum += physicalData.height.toDouble() }
             return sum / dat.size
         }
 
         // 시력 분포를 구함
-        fun distVision(dat: ArrayList<PhysicalData>, dist: Array<Int>) {
-
+        fun distVision(dat: Array<PhysicalData>, dist: IntArray) {
+            dist[0] = 0
+            for (i in dat.indices)
+                if (dat[i].vision >= 0.0 && dat[i].vision <= VMAX / 10.0)
+                    dist[(dat[i].vision * 10).toInt()]++
         }
     }
 }
 
 fun main(array: Array<String>) {
+    val input = Scanner(System.`in`)
 
+    val physicalDataList = arrayOf(
+        PhysicalExamination.PhysicalData("박현규", 162, 0.3),
+        PhysicalExamination.PhysicalData("함진아", 173, 0.7),
+        PhysicalExamination.PhysicalData("최윤미", 175, 2.0),
+        PhysicalExamination.PhysicalData("홍연의", 171, 1.5),
+        PhysicalExamination.PhysicalData("이수진", 168, 0.4),
+        PhysicalExamination.PhysicalData("김영준", 174, 1.2),
+        PhysicalExamination.PhysicalData("박용규", 169, 0.8),
+    )
+
+    println("평균 키 : ${PhysicalExamination.aveHeight(physicalDataList)}")
+
+    println("시력 분포")
+    val distResult = IntArray(PhysicalExamination.VMAX)
+    PhysicalExamination.distVision(physicalDataList, distResult)
+    for (i in distResult.indices) println("${i / 10.0} ~ : ${distResult[i]}")
 }
